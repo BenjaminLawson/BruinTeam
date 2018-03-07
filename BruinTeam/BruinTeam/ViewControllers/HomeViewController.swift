bce329744cc50246c017f1f3248340c9c5bb9d6f
briansblee <briansblee@g.ucla.edu>
2018-03-06 21:19:59 -0800

    Added bgm(no copyright music)

--- BruinTeam/BruinTeam/ViewControllers/HomeViewController.swift
+++ BruinTeam/BruinTeam/ViewControllers/HomeViewController.swift
@@ -1,13 +1,28 @@
 import UIKit
+import AVFoundation
 
 class HomeViewController: UIViewController {
 
+    var audioPlayer:AVAudioPlayer!
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let audioFilePath = Bundle.main.path(forResource: "argsound_alpha-bit", ofType: "mp3")
+        
+        if audioFilePath != nil{
+            let audioFileUrl = NSURL.fileURL(withPath: audioFilePath!)
+            audioPlayer = try! AVAudioPlayer(contentsOf: audioFileUrl)
+            audioPlayer.play()
+        }
+        else{
+            print("couldn't load audio player :/")
+        }
     }
 
     override func viewDidAppear(_ animated: Bool) {
         navigationController?.setNavigationBarHidden(false, animated: false)
     }
+    
 }
+
 
