98867ae124e003b0a0405543bfe494f81fa2d38b
Lawrence Jung <lawrencejung92@gmail.com>
2018-03-07 18:33:07 -0800

    Added background for GameOverViewScene

--- BruinTeam/BruinTeam/ViewControllers/GameOverViewController.swift
+++ BruinTeam/BruinTeam/ViewControllers/GameOverViewController.swift
@@ -10,9 +10,36 @@ class GameOverViewController: UIViewController {
         super.viewDidLoad()
         
         gameOverLabel.text = didWin ? "You won!!!" : "You lost!!!"
+
+        if (didWin) {
+            self.view.addBackground(imageName: "ucla")
+        } else {
+            self.view.addBackground(imageName: "usc")
+        }
     }
     
     @IBAction func startOverButtonPressed(_ sender: Any) {
         self.navigationController?.popToRootViewController(animated: true)
+    }
+}
+
+extension UIView {
+    func addBackground(imageName: String = "ucla", contentMode: UIViewContentMode = .scaleToFill) {
+        // setup the UIImageView
+        let backgroundImageView = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImageView.image = UIImage(named: imageName)
+        backgroundImageView.contentMode = contentMode
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(backgroundImageView)
+        sendSubview(toBack: backgroundImageView)
+        
+        // adding NSLayoutConstraints
+        let leadingConstraint = NSLayoutConstraint(item: backgroundImageView, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1.0, constant: 0.0)
+        let trailingConstraint = NSLayoutConstraint(item: backgroundImageView, attribute: .trailing, relatedBy: .equal, toItem: self, attribute: .trailing, multiplier: 1.0, constant: 0.0)
+        let topConstraint = NSLayoutConstraint(item: backgroundImageView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1.0, constant: 0.0)
+        let bottomConstraint = NSLayoutConstraint(item: backgroundImageView, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1.0, constant: 0.0)
+        
+        NSLayoutConstraint.activate([leadingConstraint, trailingConstraint, topConstraint, bottomConstraint])
     }
 }
